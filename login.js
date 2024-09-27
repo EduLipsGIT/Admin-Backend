94a6e2b7196eacaac29b71a527edb388bc9e561a
KirtimanNanda <173339421+EduLips@users.noreply.github.com>
2024-09-27 16:15:28 +0530

    Callback urls

--- login.js
+++ login.js
@@ -32,8 +32,10 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
-}, 
+  callbackURL: process.env.NODE_ENV === 'production'
+  ? 'https://author.edulips.com/auth/google/callback'
+  : 'http://localhost:3000/auth/google/callback'
+},
 async (accessToken, refreshToken, profile, done) => {
   try {
     // Use Google profile.id as UID (this is a string)
