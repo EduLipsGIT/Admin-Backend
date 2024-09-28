74c28b60caee17b9a9fa661e2fe9a8561e256e54
KirtimanNanda <173339421+EduLips@users.noreply.github.com>
2024-09-28 15:42:33 +0530

    PLS

--- index.js
+++ index.js
@@ -50,7 +50,9 @@ app.use(passport.session());
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: 'https://author.edulips.com/auth/google/callback'
+  
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     // Use Google profile.id as UID (this is a string)
