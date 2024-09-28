a2b5b7e1fd467b816a3e4ab560eec3ce678b29ef
KirtimanNanda <173339421+EduLips@users.noreply.github.com>
2024-09-28 14:13:17 +0530

    Up

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'em
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'https://author.edulips.com'
+  callbackURL: 'https://author.edulips.com/index.html'
   
 }, async (accessToken, refreshToken, profile, done) => {
   try {
