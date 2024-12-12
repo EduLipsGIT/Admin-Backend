5ad0177ad18023ec55acbe8feb453ebb3954e201
Kirtiman_EduLips <kirtiman.nanda35@gmailcom>
2024-12-12 10:34:26 +0530

    admin

--- index.js
+++ index.js
@@ -872,7 +872,7 @@ const validCredentials = [
   { username: 'Kirtiman Nanda', password: 'Kirtiman_Pass' },
   { username: 'Sonam Kumari', password: 'Sonam_Pass2024' },
   { username: 'Navjyoti Kumar', password: 'Navjyoti_Pass' },
-  { username: 'Pramod Sharma', password: 'pramod_edulips2024' },
+  { username: 'Pramod Kumar', password: 'pramod_edulips2024' },
 ];
 
 app.post('/validate_login', async (req, res) => {
